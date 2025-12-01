6f5f99e3006afdd8d8caf40d687cd0b4d9869b0a
skawa <skawa@example.com>
2025-12-01 17:15:28 +0900

    マイクラURL更新

--- stores/minecraftSdgsStore.ts
+++ stores/minecraftSdgsStore.ts
@@ -179,6 +179,7 @@ const defaultStageDetails: Record<number, MinecraftStageProgress> = {
     sdgsWorkUrl: 'https://youtu.be/C_gTPDP2SrU',
     sdgsFormUrl: 'https://forms.gle/GQi9eYYrc4vPkDQWA',
     sdgsSupportPrintUrl: 'https://www.canva.com/design/DAG5yEP0dFc/4VkXyVPAlt9Ox2StMoyWKw/edit?utm_content=DAG5yEP0dFc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton',
+    sdgsStageDownloadUrl: 'https://xgf.nu/fkkt3',
     programmingWorkUrl: 'https://youtu.be/sample4-programming',
     programmingFormUrl: 'https://forms.gle/sample4-programming',
     programmingSupportPrintUrl: 'https://example.com/stage-4-programming-support',
